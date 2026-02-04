033c88c485d1fb94b9c2b43e14e73cecd88a7d58
raju <rajuchavan14599@gmail.com>
2026-02-04 22:57:11 +0530

    button made scale on click

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -3,7 +3,7 @@ import React from 'react';
 const Button = ({ children, onClick, className }) => {
   return (
     <button
-      className={`w-full bg-[var(--col1)] text-white py-3 rounded-md mt-4 hover:bg-[var(--col2)] font-medium ${className}`}
+      className={`w-full bg-[var(--col1)] active:scale-95 text-white py-3 rounded-md mt-4 hover:bg-[var(--col2)] font-medium ${className}`}
       onClick={onClick}
     >
       {children}
